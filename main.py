9d18600b55de2c9884dbe477640481820e28f8e3
andrewimpellitteri <andrewimpellitteri@gmail.com>
2024-01-19 16:40:13 -0500

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+from manifoldpy import api
+from manifoldpy.api import BinaryMarket
+
+for market in api.get_all_markets(limit=300):
+
+    if isinstance(market, BinaryMarket):
+        print(market.question)
+
+    print("\n")
