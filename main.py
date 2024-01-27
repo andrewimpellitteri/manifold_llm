20709dcdc43f6b30a9d30f8dd3a0b0342fa0d387
andrewimpellitteri <andrewimpellitteri@gmail.com>
2024-01-27 15:37:39 -0500

    changed question to class

--- main.py
+++ main.py
@@ -3,22 +3,48 @@ from manifoldpy.api import BinaryMarket
 from llama_cpp import Llama
 from chatformat import format_chat_prompt
 import numpy as np
-import pandas as pd
 import os
+import pickle
+
 
 test_model_path = "/Users/andrew/Documents/dev/text-generation-webui/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
-# llm = Llama(model_path=test_model_path, use_mlock=True, logits_all=True)
-df_list = []
+question_list_fname = "question_list.pkl"
 
-if os.path.exists("results.csv"):
-    df = pd.read_csv("results.csv")
-    print(df.columns)
+class Question:
+    def __init__(self, qid, question, market_probability):
+        self.qid = qid
+        self.question = question
+        self.market_probability = market_probability
+        self.models = {}
+
+    def add_model(self, model_name, model_prob):
+        self.models[model_name] = model_prob
+
+    def __str__(self):
+        models = "\n".join([f"{key}: {value}" for key, value in self.models.items()])
+        return f"Question ID: {self.qid}\nQuestion: {self.question}\nMarket Probability: {self.market_probability}\n{models}"
+
+
+# Save question_list to a file using pickle
+def save_question_list(question_list, file_path):
+    with open(file_path, "wb") as file:
+        pickle.dump(question_list, file)
+
+
+# Load question_list from a file using pickle
+def load_question_list(file_path):
+    if not os.path.exists(file_path):
+        return []
+
+    with open(file_path, "rb") as file:
+        return pickle.load(file)
 
 
 def logprob_convert(logprob):
     return 1 / (1 + np.exp(-logprob))
 
-def process_question(question, llm, prompt_format='llama-2'):
+
+def process_question(question, llm, prompt_format="llama-2"):
     # sys_prompt = "You are a helpful assistant who is trying to help me bet on a prediction market and always answers with a single word, either yes or no. You will only respond to the question with a single word, either yes or no. You will be penalized if you do not respond with yes or no. If you are unsure you must respond with the option you think has the higher probability."
     alt_sys = "You are a helpful assistant specializing in predicting outcomes. Always respond with a single word, either 'yes' or 'no.' You will be penalized for deviating. If unsure, choose the option with the higher probability based on recent information."
 
@@ -33,19 +59,18 @@ def process_question(question, llm, prompt_format='llama-2'):
     output = llm(prompt, max_tokens=32, stop=stop, logprobs=True, temperature=0)
     output_text = output["choices"][0]["text"]
 
-    print('output text ', output_text)
+    print("output text ", output_text)
 
     prob = None
-    if 'yes' in output_text.lower() or 'no' in output_text.lower():
-        token_probs = output["choices"][0]['logprobs']['top_logprobs']
+    if "yes" in output_text.lower() or "no" in output_text.lower():
+        token_probs = output["choices"][0]["logprobs"]["top_logprobs"]
 
         # Iterate through the keys of the dictionary
         for entry in token_probs:
-
             key, result = entry.popitem()
-            if 'yes' in key.lower():
+            if "yes" in key.lower():
                 # Get the result for the key
-                
+
                 prob = logprob_convert(result)
 
             else:
@@ -54,78 +79,51 @@ def process_question(question, llm, prompt_format='llama-2'):
             return prob
 
 
-def get_questions(qids):
-    return [api.get_market(market_id=qid) for qid in qids]
-
-def add_new_model_to_csv(model_path, df, add_questions=False, question_limit=1000, prompt_format='llama-2'):
-
+def run_model_on_questions(model_path, question_list, prompt_format="llama-2"):
     llm = Llama(model_path=model_path, use_mlock=True, logits_all=True)
 
-    if add_questions:
+    model_name = os.path.basename(model_path)
 
-        for market in api.get_all_markets(limit=question_limit):
-            if isinstance(market, BinaryMarket):
+    for question in question_list:
+        model_prob = process_question(
+            question.question, llm, prompt_format=prompt_format
+        )
 
-                if market.id in df['qid']:
-                    continue
-            
-                prob = process_question(market.question, llm, prompt_format='llama-2')
+        if model_prob is not None:
+            print(question)
 
-                if prob is not None:
-                    print(f"llm prob: {prob}")
-                    model_col = os.path.basename(test_model_path)
+            print(f"llm {model_name}: {model_prob}")
 
-                    row = [{"qid": market.id, "market": market.probability, model_col: prob}]
+            question.models[model_name] = model_prob
 
-                    temp_df = pd.DataFrame(row)
-
-                    pd.concat([df, temp_df])
-
-                    print(df)
-
-                    df.to_csv("results.csv", index=False)
+    save_question_list(question_list, question_list_fname)
 
 
-    for question in get_questions(df['qid'].tolist()):
-
-        prob = process_question(question.question, llm, prompt_format=prompt_format)
-
-        if prob is not None:
-            print(f"llm: {prob}")
-
-            df.loc[df['qid'] == question.id, os.path.basename(model_path)] = prob
-            df.loc[df['qid'] == question.id, 'market'] = question.probability
-            print('saving..')
-            df.to_csv('results.csv', index=False)
-
-
-def create_df(limit):
-    for market in api.get_all_markets(limit=limit):
+def add_questions(question_list, prompt_format="llama-2", question_limit=1000
+):
+    for market in api.get_all_markets(limit=question_limit):
+        print(market)
         if isinstance(market, BinaryMarket):
+            
+            existing_question = next((question for question in question_list if question.qid == market.id), None)
 
-            # print(market.id)
-            print(f"Question: {market.question}, market prob: {market.probability}")
-            # print(market.probability)
-            # print(market.createdTime)
-            # print(market)
-            # print("\n")
+            if existing_question is not None:
+                # Update market probability if it has changed
+                if existing_question.market_probability != market.probability:
+                    existing_question.market_probability = market.probability
+                    print(f"Updated market probability for Question ID {market.id}: {market.probability}")
 
-            prob = process_question(market.question)
+                continue
 
-            if prob is not None:
-                print(f"llm prob: {prob}")
-                model_col = os.path.basename(test_model_path)
+            question_list.append(
+                Question(market.id, market.question, market.probability)
+            )
 
-                row = {"qid": market.id, "market": market.probability, model_col: prob}
-                df_list.append(row)
-                
-                df = pd.DataFrame(df_list)
-
-                df.to_csv("results.csv", index=False)
-
-                print(df)
+    save_question_list(question_list, file_path=question_list_fname)
 
 
+question_list = load_question_list(question_list_fname)
 
-new_model_path = "/Users/andrew/Documents/dev/text-generation-webui/models/openchat_3.5.Q4_K_M.gguf"
-add_new_model_to_csv(new_model_path, df, add_questions=True)
+add_questions(question_list, prompt_format='llama-2')
+
+run_model_on_questions(test_model_path, question_list, prompt_format='llama-2')
